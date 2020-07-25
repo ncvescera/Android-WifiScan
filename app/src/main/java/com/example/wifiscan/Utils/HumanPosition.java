7a894c479cfb908551bd4fe8c550f18b681daf06
Nicolò Vescera <nicolo.vescera@gmail.com>
2020-07-25 16:36:24 +0200

    Refactoring HumanPosition
    Commented code

--- app/src/main/java/com/example/wifiscan/Utils/HumanPosition.java
+++ app/src/main/java/com/example/wifiscan/Utils/HumanPosition.java
@@ -10,10 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-// Classe per convertire delle coordinate (Double) in una Stringa Human-Readable (String)
 public class HumanPosition {
-    private Double lon;
-    private Double lat;
     private Context context;
     private Geocoder geoCoder;
     private StringBuilder builder;
@@ -21,20 +18,18 @@ public class HumanPosition {
     private String position;
 
     public HumanPosition(Context context) {
-        this.lat = lat;
-        this.lon = lon;
         this.context = context;
 
-        // inizializza l'attrezzo che trasformerà le coordinate in una stringa
-        geoCoder = new Geocoder(context, Locale.getDefault()); //it is Geocoder
+        // init. the thing that will commute coordinate in to a human readable string
+        geoCoder = new Geocoder(this.context, Locale.getDefault()); //it is Geocoder
         builder = new StringBuilder();
     }
 
     public String coordToString(Double lat, Double lon) {
-        // prova la conversione, in caso di errore la stringa sarà nulla ("")
+        // trying the conversion, if error occur the string will be ""
         try {
-            // tutta sta roba si fa perchè l'indirizzo potrebbe essere troppo lungo e quindi viene diviso in varie righe (?)
-            // di solito dentro la lista address c'è solo un elemento
+            // usually address List has only an element but I have to do all this thing because it could have more than 1 elem
+            // not really sure
             List<Address> address = geoCoder.getFromLocation(lat, lon, 1);
 
             int maxLines = address.get(0).getMaxAddressLineIndex();
@@ -44,7 +39,7 @@ public class HumanPosition {
                 builder.append(" ");
             }
 
-            this.position = builder.toString(); // sarà la stringa finale
+            this.position = builder.toString(); // return string
 
         } catch (IOException e) {
             Log.d("TEST_COORD", "ERRIR:IO");
@@ -58,18 +53,18 @@ public class HumanPosition {
     }
 
     public ArrayList<Double> stringToCoord(String nome) {
-        // inizializzo l'array dove andranno a finire i dati
+        // init result ArrayList
         ArrayList<Double> result = new ArrayList<Double>();
 
         try {
-            // prova ad effettuare la conversione
+            // trying to do the conversion
             List<Address> address = geoCoder.getFromLocationName(nome, 1);
 
             int maxLines = address.get(0).getMaxAddressLineIndex();
 
             Log.d("TEST", ""+maxLines);
 
-            // aggiunge all'array prima la latitudine e poi la longitudine
+            // adding latitude and longitude to the array
             result.add(address.get(0).getLatitude());
             result.add(address.get(0).getLongitude());
 
