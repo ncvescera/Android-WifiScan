64bfe7ac5faeff399fe277353b2de004a32c4b26
Nicolò Vescera <nicolo.vescera@gmail.com>
2020-08-05 16:00:14 +0200

    Code Refactoring
    Spostati i binding dei Listenere in una funzione apposita per rendere il codice più leggibile

--- app/src/main/java/com/example/wifiscan/DbActivity.java
+++ app/src/main/java/com/example/wifiscan/DbActivity.java
@@ -15,7 +15,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.SearchView;
 import android.widget.Toast;
 
 
@@ -52,6 +51,47 @@ public class DbActivity extends AppCompatActivity {
 
         // listview principale
         listView = (ListView) findViewById(R.id.cursor_listview);
+
+        // inizializzo il dbManager
+        manager = DBManager.getDbInstance(getApplicationContext());
+
+        // prendo tutti i dati dal database per inizializzare la listview
+        cursor = manager.query();
+
+        // creo l'adapter e lo aggiungo alla listview
+        adapter = new WifiCursorAdapter(this, cursor, 0);
+        listView.setAdapter(adapter);
+
+        // aggiunge i listeners ai vari oggetti
+       setListeners();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // prendo il riferimento del layout del menu
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.db_menu, menu);
+
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.deleteDatabase:
+                manager.deleteAllDataTable(DBStrings.TBL_NAME);
+                cursor = null;
+
+                adapter.changeCursor(cursor);
+                adapter.notifyDataSetChanged();
+                break;
+        }
+
+        return true;
+    }
+
+    private void setListeners() {
+        /** LISTENER PER LA LISTVIEW listView**/
         // con una pressione continua sulla riga si può eliminare
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
@@ -67,15 +107,22 @@ public class DbActivity extends AppCompatActivity {
             }
         });
 
-        // inizializzo il dbManager
-        manager = DBManager.getDbInstance(getApplicationContext());
+        /** LISTENER PER IL BOTTONE cercaBtn **/
+        // con una lunga pressione del tasto viene eliminato il contenuto delle EditText
+        cercaBtn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                SSIDEditText.setText("");
+                PositionEditText.setText("");
 
-        // prendo tutti i dati dal database per inizializzare la listview
-        cursor = manager.query();
+                cursor = manager.query();
 
-        // creo l'adapter e lo aggiungo alla listview
-        adapter = new WifiCursorAdapter(this, cursor, 0);
-        listView.setAdapter(adapter);
+                adapter.changeCursor(cursor);
+                adapter.notifyDataSetChanged();
+
+                return true;
+            }
+        });
 
         // esegue la ricerca in base al contenuto delle EditText
         cercaBtn.setOnClickListener(new View.OnClickListener() {
@@ -131,52 +178,7 @@ public class DbActivity extends AppCompatActivity {
                         Toast.makeText(contesto, "Posizione inesistente :/", Toast.LENGTH_SHORT).show();
                     }
                 }
-
-                // aggiungere alertbox
-                //manager.deleteAllDataTable(DBStrings.TBL_NAME);
-                //listView.setAdapter(null);
-
             }
         });
-
-        // con una lunga pressione del tasto viene eliminato il contenuto delle EditText
-        cercaBtn.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                SSIDEditText.setText("");
-                PositionEditText.setText("");
-
-                cursor = manager.query();
-
-                adapter.changeCursor(cursor);
-                adapter.notifyDataSetChanged();
-
-                return true;
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // prendo il riferimento del layout del menu
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.db_menu, menu);
-
-
-        return true;
-    }
-
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.deleteDatabase:
-                manager.deleteAllDataTable(DBStrings.TBL_NAME);
-                cursor = null;
-
-                adapter.changeCursor(cursor);
-                adapter.notifyDataSetChanged();
-                break;
-        }
-
-        return true;
     }
 }
